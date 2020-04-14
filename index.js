5b62389399e7178183f509c25f3f18a9b5d19d92
specter25 <ujjwalcoding012@gmail.com>
2020-04-14 21:42:12 +0530

    oauth implementation 10

--- index.js
+++ index.js
@@ -137,8 +137,7 @@ app.get('/auth/redirect', (req, res) =>{
       uri: 'https://slack.com/api/oauth.access?code='
           +req.query.code+
           '&client_id='+process.env.SLACK_CLIENT_ID+
-          '&client_secret='+process.env.SLACK_CLIENT_SECRET+
-          '&redirect_uri='+process.env.REDIRECT_URI,
+          '&client_secret='+process.env.SLACK_CLIENT_SECRET,
       method: 'GET'
   }
   request(options, (error, response, body) => {
